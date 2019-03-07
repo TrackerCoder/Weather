6edfa1958a8ecc61da6857ff316f305da97fda7a
Tracker <>
2019-03-07 14:59:18 +0300

    Init app

---
+++ Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Tracker on 07.03.2019.
+//  Copyright © 2019 Tracker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
